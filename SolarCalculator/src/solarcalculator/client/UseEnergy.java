edb628ba9fa882343baf60b88278abc3b9452697
VANISHBACK <VanishBack@gmail.com>
2012-09-10 14:03:53 +1000

    add new UserEnergy class and its test

---
+++ SolarCalculator/src/solarcalculator/client/UseEnergy.java
@@ -0,0 +1,60 @@
+package solarcalculator.client;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * Use Energy include all the function need to have in Use Energy Tab.
+ * @author grey
+ *
+ */
+
+public class UseEnergy {
+	private Double EnergyUse;
+	private Double YearlyEnergyUse;
+	// show yearly energy use Result
+	public Double getYearlyEnergyUse() {
+		return YearlyEnergyUse;
+	}
+	public void setYearlyEnergyUse(int selection) {
+		if(selection == 0)
+		{
+				YearlyEnergyUse = EnergyUse * 365;
+		}
+		else if(selection == 1)
+		{
+				YearlyEnergyUse = EnergyUse * 12;
+		}
+		else if(selection == 2)
+		{
+				YearlyEnergyUse = EnergyUse * 4;
+		}
+		else
+		{
+			return;
+		}
+	}
+	// input usage
+	public void setEnergyUse(String input)
+	{
+
+		// TODO Auto-generated method stub
+		if (!input.matches("^[0-9]*$")) {
+			Window.alert("'" + input + "' is not a valid symbol.");
+
+			return;
+		}	
+		
+		EnergyUse = Double.parseDouble(input);
+	}
+	
+	public Double getEnergyUse(){
+		return EnergyUse;
+	}
+	// show average import tariff result
+
+	
+	// input 
+	
+	// input rate
+}
---
+++ SolarCalculator/test/solarcalculator/client/UseEnergyTest.java
@@ -0,0 +1,41 @@
+package solarcalculator.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gwt.user.client.ui.TextBox;
+
+public class UseEnergyTest {
+	private UseEnergy instance;
+
+	@Before
+	public void setUp() throws Exception {
+		instance= new UseEnergy();
+	}
+
+	@Test
+	public void testGetYearlyEnergyUse() {
+		instance.setEnergyUse("120.00");
+		instance.setYearlyEnergyUse(2);
+		System.out.println(instance.getYearlyEnergyUse().toString());
+	}
+
+	@Test
+	public void testSetYearlyEnergyUse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnergyUse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnergyUse() {
+		instance.setEnergyUse("123.00");
+		System.out.println(instance.getEnergyUse());
+	}
+
+}
